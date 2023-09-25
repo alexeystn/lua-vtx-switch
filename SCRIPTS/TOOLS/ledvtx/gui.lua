21e964220743bf5c098dfb30e1384e166b40291e
Alexey Stankevich <alexey.stn@gmail.com>
2023-09-25 23:23:13 +0300

    Add color screen support

--- SCRIPTS/TOOLS/ledvtx/gui.lua
+++ SCRIPTS/TOOLS/ledvtx/gui.lua
@@ -1,36 +1,56 @@
 local LCD_C = LCD_W / 2 + 1
 
+local w = 0  -- large screen flag
+if LCD_H > 96 then w = 1 end
+
 
 local function drawArrow(x, y, dir)
-  for i = 0, 4 do
-    lcd.drawLine(x+i*dir, y-i, x+i*dir, y+i, SOLID, FORCE)
+  for i = 0, 4+w*6 do
+    lcd.drawLine(x+i*dir, y-i, x+i*dir, y+i, SOLID, 0)
   end
 end
 
 
 local function drawSelector(pos, text, isSelected, isActive)
-  local flags = 0 
-  local offset = math.floor(string.len(text) * 7 / 2) + 1
-  if isSelected then
-    flags = INVERS
-    if isActive then
-      drawArrow(LCD_C-57, 18*(pos-1)+13, 1) 
-      drawArrow(LCD_C+56, 18*(pos-1)+13, -1) 
+  local flags = 0
+  local offset = 0
+  if w == 0 then
+    flags = MIDSIZE 
+    offset = math.floor(string.len(text) * 7 / 2) + 1
+    if isSelected then
+      flags = flags + INVERS
+      lcd.drawFilledRectangle(LCD_C-50, 18*(pos-1)+5, 100, 17, SOLID)
     end
-    lcd.drawFilledRectangle(LCD_C-50, 18*(pos-1)+5 , 100, 17, SOLID)
+  else 
+    flags = DBLSIZE + CENTER
+    if isSelected then 
+      lcd.drawRectangle(LCD_C-120, 50*(pos-1)+60, 240, 44, SOLID, 2)
+    end
   end
-  lcd.drawText(LCD_C-offset, (pos-1)*18+7 , text, flags + MIDSIZE)
+  if isActive and isSelected then
+    drawArrow(LCD_C-57-83*w, (18+32*w)*(pos-1)+13+68*w, 1)
+    drawArrow(LCD_C+56+83*w, (18+32*w)*(pos-1)+13+68*w, -1) 
+  end
+  lcd.drawText(LCD_C-offset, (pos-1)*(18+32*w)+7+57*w, text, flags)
 end
 
 
 local function drawButton(text, isSelected)
-  local flag = 0
-  local offset = math.floor((string.len(text)*5)/2)
-  if isSelected then
-    flag = INVERS
-    lcd.drawFilledRectangle(LCD_C-30, 46 , 60, 12, SOLID)
+  local flags = 0
+  local offset = 0
+  if w == 0 then
+    offset = math.floor((string.len(text)*5)/2)
+    if isSelected then
+      flags = INVERS
+      lcd.drawFilledRectangle(LCD_C-30, 46, 60, 12, SOLID)
+    end
+  else
+    flags = MIDSIZE + CENTER
+    if isSelected then 
+      lcd.drawRectangle(LCD_C-50, 169, 100, 32, SOLID, 2)
+    end
   end
-  lcd.drawText(LCD_C-offset, 48, text, flag)  
+  lcd.drawText(LCD_C-offset, 48+122*w, text, flags) 
 end
 
 
