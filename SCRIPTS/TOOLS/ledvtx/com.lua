aa864b1ce1cf057016a97c26558145031a1f7a4f
Alexey Stankevich <alexey.stn@gmail.com>
2025-01-09 22:44:55 +0300

    Use Larson scanner

--- SCRIPTS/TOOLS/ledvtx/com.lua
+++ SCRIPTS/TOOLS/ledvtx/com.lua
@@ -87,10 +87,12 @@ end
 local function prepareLedCommand(color, n, version)
   local cmd = {}
   cmd.header = MSP_SET_LED_STRIP
+  enableLarsonScanner = 0
+  
   if version < 46 then
-    cmd.payload = { n-1, (n-1)*16, 0, color*4, 0 }
+    cmd.payload = { n-1, (n-1)*16, 16*enableLarsonScanner, color*4, 0 }
   else 
-    cmd.payload = { n-1, (n-1)*16, 0, bit32.lshift(bit32.band(color, 0x03), 6), bit32.rshift(color, 2)}
+    cmd.payload = { n-1, (n-1)*16, 16*enableLarsonScanner, bit32.lshift(bit32.band(color, 0x03), 6), bit32.rshift(color, 2)}
   end
   cmd.write = true
   cmd.text = "Switching LED " .. tostring(n)
