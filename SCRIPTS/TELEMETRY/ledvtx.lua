646db58eda1224dc2b7e93a49a2c6e2e59d97f51
Alexey Stankevich <alexey.stn@gmail.com>
2025-05-18 23:32:33 +0300

    Disable background script

--- SCRIPTS/TELEMETRY/ledvtx.lua
+++ SCRIPTS/TELEMETRY/ledvtx.lua
@@ -1,3 +1,3 @@
 local script = assert(loadScript("/SCRIPTS/TOOLS/ledvtx.lua"))()
 
-return { run=script.run, background=script.background, init=script.init}
+return { run=script.run, init=script.init}
