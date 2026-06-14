6a5ac82409f5dc4522ff3a8ff5b45f0ec79b92c4
Alexey Stankevich <alexey.stn@gmail.com>
2026-06-14 22:29:53 +0300

    Reverse merge commit

--- SCRIPTS/TOOLS/ledvtx.lua
+++ SCRIPTS/TOOLS/ledvtx.lua
@@ -15,27 +15,19 @@ local ITEM_POWER = 5
 local ITEM_COUNT = 6
 local ITEM_LARSON = 7
 local ITEM_VERSION = 8
-local ITEM_VTX_MODE = 9
-local ITEM_BANDS = 10
 
 local IDLE=1
 local BUSY=2
 local DONE=3
 local FAIL=4
 
-local VTX_MODE_MSP = 1
-local VTX_MODE_ELRS = 2
-
-local BANDS_ANALOG = 1
-local BANDS_HDZERO = 2
-
 local maxLedCount = 32
 
 local colorLabels = { "Red", "Orange", "Yellow", "Green", "Cyan", "Blue", "Violet", "Magenta", "White", "Black", "   * * * *" }
 local colorIds = { 2, 3, 4, 6, 8, 10, 11, 12, 1, 0, nil }
 
-local bandNames = { "Band A", "Band B", "Band E", "Fatshark", "Raceband", "Lowband"}
-local bandIds = { 1, 2, 3, 4, 5, 6 }
+local bandNames = { "Raceband", "Fatshark", "Lowband"}
+local bandIds = { 5, 4, 6 }
 
 local switchLabels = {"OFF", "ON"}
 local switchIds = {0, 1}
@@ -43,18 +35,10 @@ local switchIds = {0, 1}
 local versionLabels = {"4.5+", "4.4-"}
 local versionIds = {0, 1}
 
-local vtxModeLabels = {"MSP", "ELRS"}
-local vtxModeIds = {VTX_MODE_MSP, VTX_MODE_ELRS}
-
-local bandsLabels = {"Analog", "HDZero"}
-local bandsIds = {BANDS_ANALOG, BANDS_HDZERO}
-
 local powerLabels = {}
 local powerIds = {}
-powerLabels[#powerLabels+1] = "-"
-powerIds[#powerIds+1] = 0
-for i = 1, 8 do
-  powerLabels[#powerLabels+1] = tostring(i)
+for i = 0, 4 do
+  powerLabels[#powerLabels+1] = tostring(i+1)
   powerIds[#powerIds+1] = i
 end
 
@@ -65,31 +49,16 @@ for i = 1, maxLedCount do
   countIds[#countIds+1] = i
 end
 
-local analogChannels = {}
-local hdzeroChannels = {}
 local channelLabels = {}
 local channelIds = {}
-
-local function addChannel(list, band, channel)
-  list[#list+1] = {band, channel}
-end
-
-for iBand = 1, #bandNames do
+for iBand = 1, 3 do
   for iCh = 1, 8 do
-    addChannel(analogChannels, bandIds[iBand], iCh)
+    channelLabels[#channelLabels+1] = bandNames[iBand] .. " " .. tostring(iCh)
+    channelIds[#channelIds+1] = {bandIds[iBand], iCh}
   end
 end
-
-for iCh = 1, 8 do
-  addChannel(hdzeroChannels, 5, iCh)
-end
-addChannel(hdzeroChannels, 3, 1)
-addChannel(hdzeroChannels, 4, 1)
-addChannel(hdzeroChannels, 4, 2)
-addChannel(hdzeroChannels, 4, 4)
-for iCh = 1, 8 do
-  addChannel(hdzeroChannels, 6, iCh)
-end
+channelLabels[#channelLabels+1] = "   * * * *"
+channelIds[#channelIds+1] = {nil, nil}
 
 menu = {}
 
@@ -99,85 +68,12 @@ menu[ITEM_POWER] = {labels = powerLabels, values = powerIds, pos = 1}
 menu[ITEM_COUNT] = {labels = countLabels, values = countIds, pos = 1}
 menu[ITEM_LARSON] = {labels = switchLabels, values = switchIds, pos = 1}
 menu[ITEM_VERSION] = {labels = versionLabels, values = versionIds, pos = 1}
-menu[ITEM_VTX_MODE] = {labels = vtxModeLabels, values = vtxModeIds, pos = 1}
-menu[ITEM_BANDS] = {labels = bandsLabels, values = bandsIds, pos = 1}
 
 
 local menuPosition = ITEM_LED
 local isItemActive = false
 local isOptionsMenuActive = false
 local state = IDLE
-local vtxConfigVersion = nil
-local statusText = nil
-
-
-local function getVtxMode()
-  return menu[ITEM_VTX_MODE].values[menu[ITEM_VTX_MODE].pos]
-end
-
-
-local function getBandsMode()
-  return menu[ITEM_BANDS].values[menu[ITEM_BANDS].pos]
-end
-
-
-local function getBandName(band)
-  for i = 1, #bandIds do
-    if bandIds[i] == band then
-      return bandNames[i]
-    end
-  end
-  return "Band " .. tostring(band)
-end
-
-
-local function channelLabel(band, channel)
-  if band and channel then
-    return getBandName(band) .. " " .. tostring(channel)
-  end
-  return "   * * * *"
-end
-
-
-local function fillChannelList(currentBand, currentChannel)
-  local source = getBandsMode() == BANDS_HDZERO and hdzeroChannels or analogChannels
-  channelLabels = {}
-  channelIds = {}
-
-  for i = 1, #source do
-    local band = source[i][1]
-    local channel = source[i][2]
-    channelLabels[#channelLabels+1] = channelLabel(band, channel)
-    channelIds[#channelIds+1] = {band, channel}
-  end
-
-  channelLabels[#channelLabels+1] = "   * * * *"
-  channelIds[#channelIds+1] = {nil, nil}
-
-  if currentBand and currentChannel then
-    for i = 1, #channelIds do
-      if channelIds[i][1] == currentBand and channelIds[i][2] == currentChannel then
-        menu[ITEM_VTX].labels = channelLabels
-        menu[ITEM_VTX].values = channelIds
-        menu[ITEM_VTX].pos = i
-        return
-      end
-    end
-
-    channelLabels[#channelLabels+1] = channelLabel(currentBand, currentChannel)
-    channelIds[#channelIds+1] = {currentBand, currentChannel}
-    menu[ITEM_VTX].pos = #channelLabels
-  end
-
-  menu[ITEM_VTX].labels = channelLabels
-  menu[ITEM_VTX].values = channelIds
-  if menu[ITEM_VTX].pos > #channelLabels then
-    menu[ITEM_VTX].pos = #channelLabels
-  end
-end
-
-
-fillChannelList()
 
 
 local function itemIncrease()
@@ -200,7 +96,7 @@ end
 
 
 local function menuMoveDown()
-  if menuPosition ~= ITEM_SAVE and menuPosition ~= ITEM_BANDS then
+  if menuPosition ~= ITEM_SAVE and menuPosition ~= ITEM_VERSION then
     menuPosition = menuPosition + 1
   end
 end
@@ -213,56 +109,27 @@ local function menuMoveUp()
 end
 
 
-local function refreshStatus()
-  local event
-  statusText, event = com.getStatus()
-  if statusText then
-    state = BUSY
-  elseif event == 1 then
-    state = DONE
-  elseif event == -1 then
-    state = FAIL
-  end
-end
-
-
 local function drawDisplay()
   lcd.clear()
   if isOptionsMenuActive then
-    local firstOption = menuPosition - 3
-    if firstOption < ITEM_POWER then
-      firstOption = ITEM_POWER
-    elseif firstOption > ITEM_BANDS - 3 then
-      firstOption = ITEM_BANDS - 3
-    end
-    for row = 1, 4 do
-      local item = firstOption + row - 1
-      local label = ""
-      local offset = nil
-      if item == ITEM_POWER then
-        label = "Power Level"
-      elseif item == ITEM_COUNT then
-        label = "LED Count"
-      elseif item == ITEM_LARSON then
-        label = "Larson Scanner"
-      elseif item == ITEM_VERSION then
-        label = "BF Version"
-        offset = -4
-      elseif item == ITEM_VTX_MODE then
-        label = "VTX Mode"
-      elseif item == ITEM_BANDS then
-        label = "Bands"
-      end
-      gui.drawSmallSelector(row, label, menu[item].labels[menu[item].pos], menuPosition==item, isItemActive, offset)
-    end
+    gui.drawSmallSelector(1, "Power Level",   menu[ITEM_POWER].labels[menu[ITEM_POWER].pos], menuPosition==ITEM_POWER, isItemActive)
+    gui.drawSmallSelector(2, "LED Count",   menu[ITEM_COUNT].labels[menu[ITEM_COUNT].pos], menuPosition==ITEM_COUNT, isItemActive)
+    gui.drawSmallSelector(3, "Larson Scanner",   menu[ITEM_LARSON].labels[menu[ITEM_LARSON].pos], menuPosition==ITEM_LARSON, isItemActive)
+    gui.drawSmallSelector(4, "BF Version",   menu[ITEM_VERSION].labels[menu[ITEM_VERSION].pos], menuPosition==ITEM_VERSION, isItemActive, -4)
   else
     
     gui.drawSelector(1, colorLabels[menu[ITEM_LED].pos], menuPosition==ITEM_LED, isItemActive)
     gui.drawSelector(2, channelLabels[menu[ITEM_VTX].pos], menuPosition==ITEM_VTX, isItemActive)
 
-    local btnText = statusText
-    btnSelected = (not statusText) and (menuPosition == ITEM_SAVE)
-    if not statusText then
+    local btnText, event
+    btnText, event = com.getStatus()
+    btnSelected = (not btnText) and (menuPosition == ITEM_SAVE)
+    if not btnText then
+      if event == 1 then
+        state = DONE
+      elseif event == -1 then
+        state = FAIL
+      end
       if state == DONE then
         btnText = "Done"
       elseif state == FAIL then
@@ -270,53 +137,13 @@ local function drawDisplay()
       else
         btnText = "Save"
       end
+    else 
+      state = BUSY
     end
     gui.drawOptions(menuPosition == ITEM_OPTS)
     gui.drawButton(btnText, btnSelected)
   end
   gui.drawStatus()
-end
-
-
-local function applyVtxConfig(config_)
-  if not config_ or config_.version == vtxConfigVersion then
-    return
-  end
-  if menuPosition == ITEM_VTX or isItemActive or state ~= IDLE then
-    return
-  end
-  -- Mirror the current TX-module VTX state into the menu without forcing a write.
-  fillChannelList(config_.band, config_.channel)
-  vtxConfigVersion = config_.version
-  if config_.power then
-    for i = 1, #powerIds do
-      if powerIds[i] == config_.power then
-        menu[ITEM_POWER].pos = i
-        break
-      end
-    end
-  end
-end
-
-
-local function prepareVtxArgs()
-  return {
-    band = menu[ITEM_VTX].values[menu[ITEM_VTX].pos][1],
-    channel = menu[ITEM_VTX].values[menu[ITEM_VTX].pos][2],
-    power = menu[ITEM_POWER].values[menu[ITEM_POWER].pos],
-    vtxMode = getVtxMode()
-  }
-end
-
-
-local function sendElrsVtxConfig()
-  local args = prepareVtxArgs()
-  if getVtxMode() ~= VTX_MODE_ELRS or not args.band then
-    return
-  end
-  state = BUSY
-  config.save(menu)
-  com.sendLedVtxConfig(args)
 end
 
 
@@ -327,36 +154,28 @@ local function processEnterPress()
     return
   end
   if menuPosition ~= ITEM_SAVE and menuPosition ~= ITEM_OPTS then
-    local wasActive = isItemActive
     isItemActive = not isItemActive
-    if wasActive and menuPosition == ITEM_BANDS then
-      local current = menu[ITEM_VTX].values[menu[ITEM_VTX].pos]
-      fillChannelList(current[1], current[2])
-    end
-    if wasActive and (menuPosition == ITEM_VTX or menuPosition == ITEM_POWER) then
-      sendElrsVtxConfig()
-    end
   else
     state = BUSY
     state = DONE -- TODO: remove this line
     config.save(menu)
-
-    local args = prepareVtxArgs()
-    args.color = menu[ITEM_LED].values[menu[ITEM_LED].pos]
-    args.count = menu[ITEM_COUNT].values[menu[ITEM_COUNT].pos]
-    args.larson = menu[ITEM_LARSON].values[menu[ITEM_LARSON].pos]
-    args.version = menu[ITEM_VERSION].values[menu[ITEM_VERSION].pos]
+    
+    args = {
+      color = menu[ITEM_LED].values[menu[ITEM_LED].pos],
+      band = menu[ITEM_VTX].values[menu[ITEM_VTX].pos][1],
+      channel = menu[ITEM_VTX].values[menu[ITEM_VTX].pos][2],
+      power = menu[ITEM_POWER].values[menu[ITEM_POWER].pos],
+      count = menu[ITEM_COUNT].values[menu[ITEM_COUNT].pos],
+      larson = menu[ITEM_LARSON].values[menu[ITEM_LARSON].pos],
+      version = menu[ITEM_VERSION].values[menu[ITEM_VERSION].pos]
+    }
     com.sendLedVtxConfig(args)  -- TODO: transfer all parameters
   end
 end
 
 
 local function run_func(event)
-  com.mainLoop(getVtxMode())
-  refreshStatus()
-  if getVtxMode() == VTX_MODE_ELRS then
-    applyVtxConfig(com.getVtxConfig())
-  end
+  com.mainLoop()
   if state ~= BUSY then
     if isItemActive then
       if event == EVT_VIRTUAL_INC or event == EVT_VIRTUAL_INC_REPT then
@@ -410,8 +229,7 @@ end
 
 
 local function init_func()
-  config.load_(menu)
-  fillChannelList()
+  config.load_(menu)  
 end
 
 
--- SCRIPTS/TOOLS/ledvtx/com.lua
+++ SCRIPTS/TOOLS/ledvtx/com.lua
@@ -1,8 +1,4 @@
 local msp = assert(loadScript("msp.lua"))()
-local elrs = assert(loadScript("elrs.lua"))()
-
-local VTX_MODE_MSP = 1
-local VTX_MODE_ELRS = 2
 
 local MSP_VTX_SET_CONFIG = 89
 local MSP_EEPROM_WRITE = 250
@@ -16,10 +12,6 @@ local nextTryTime = 0
 
 local successFlag = false
 local failedFlag = false
-local transactionActive = false
-local mspResult = 1
-local elrsResult = 1
-local pendingMspCommands = nil
 
 local commandSequence = {}
 local commandPointer = 0
@@ -45,7 +37,6 @@ local function sendCurrentCommand()
   retryCount = retryCount + 1
   if retryCount > maxRetries then
     isBusy = false
-    mspResult = -1
     failedFlag = true
     return
   end
@@ -66,7 +57,6 @@ local function gotoNextCommand()
     retryCount = 0
     sendCurrentCommand()
   else
-    mspResult = 1
     successFlag = true
     currentCommand = nil
     isBusy = false
@@ -110,11 +100,8 @@ end
 
 local function prepareVtxCommand(band, channel, power)
   local cmd = {}
-  if power < 1 then
-    power = 1
-  end
   cmd.header = MSP_VTX_SET_CONFIG
-  cmd.payload = { (band-1)*8 + (channel-1), 0, power, 0 }
+  cmd.payload = { (band-1)*8 + (channel-1), 0, power+1, 0 }
   cmd.write = true
   cmd.text = "Switching VTX"
   return cmd
@@ -133,22 +120,13 @@ end
 
 local function sendLedVtxConfig(args)
   retryCount = 0
-  transactionActive = true
-  mspResult = (args.color or (args.band and args.vtxMode == VTX_MODE_MSP)) and 0 or 1
-  elrsResult = (args.band and args.vtxMode == VTX_MODE_ELRS) and 0 or 1
-  pendingMspCommands = nil
   print('Config')
   print('VTX:', args.band, args.channel)
   print('LED:', args.color, args.count, args.larson)
   print('API:', args.version)
-  print('VTX mode:', args.vtxMode)
 
   local cmd = {}
-  if args.band and args.vtxMode == VTX_MODE_ELRS then
-    -- VTX config goes to the ELRS TX module; it forwards the change itself.
-    elrs.sendVtxConfig(args)
-  elseif args.band then
-    -- Original path: write VTX directly to Betaflight over MSP.
+  if args.band then
     cmd[#cmd+1] = prepareVtxCommand(args.band, args.channel, args.power)
   end
   if args.color then
@@ -156,80 +134,47 @@ local function sendLedVtxConfig(args)
       cmd[#cmd+1] = prepareLedCommand(args.color, i, args.larson, args.version)
     end
   end
-  if #cmd > 0 then
-    cmd[#cmd+1] = prepareSaveCommand()
-  end
-  if #cmd > 0 then
-    pendingMspCommands = cmd
-    if args.vtxMode ~= VTX_MODE_ELRS or not args.band then
-      startTransmission(pendingMspCommands)
-      pendingMspCommands = nil
-    end
-  end
+  cmd[#cmd+1] = prepareSaveCommand()
+  startTransmission(cmd)
 end  
 
 
 local function getStatus()
   local text = nil
   local flag = 0
-  local elrsText, elrsEvent = elrs.getStatus()
   if isBusy then 
     if currentCommand then
       text = currentCommand.text .. " (" .. tostring(retryCount) .. ")"
     end
-  elseif elrsText then
-    text = elrsText
   end
-
-  if elrsEvent ~= 0 then
-    elrsResult = elrsEvent
-  end
-  if failedFlag or mspResult < 0 or elrsResult < 0 then
-    flag = -1
-    failedFlag = false
-    successFlag = false
-    transactionActive = false
-  elseif transactionActive and mspResult > 0 and elrsResult > 0 then
+  if successFlag then
     flag = 1
     successFlag = false
-    transactionActive = false
-  elseif successFlag then
-    successFlag = false
+  end
+  if failedFlag then
+    flag = -1
+    failedFlag = false
   end
   return text, flag
 end
 
 
-function comMainLoop(vtxMode)
+function comMainLoop()
   if isBusy then
     currentTime = getTime()
     if currentTime > nextTryTime then 
       sendCurrentCommand()
     end
-    msp.processTxQ()
-    processMspReply(msp.pollReply())
-  elseif elrs.isBusy() then
-    -- Keep pumping ELRS telemetry while it resolves discovery/read/write.
-    elrs.mainLoop()
-  else
-    -- LED MSP commands stay on the existing Betaflight path.
-    if vtxMode == VTX_MODE_ELRS then
-      elrs.mainLoop()
-    end
-    if pendingMspCommands then
-      startTransmission(pendingMspCommands)
-      pendingMspCommands = nil
-    end
   end
+  msp.processTxQ()
+  processMspReply(msp.pollReply())
 end
 
 
 function cancel()
   isBusy = false
-  pendingMspCommands = nil
-  transactionActive = false
 end
 
 
 return { sendLedVtxConfig = sendLedVtxConfig, mainLoop = comMainLoop, getStatus=getStatus, 
-  cancel=cancel, setDebug=setDebugButtonState, getVtxConfig=elrs.getVtxConfig}
+  cancel=cancel, setDebug=setDebugButtonState}
